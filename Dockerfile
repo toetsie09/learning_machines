b91b519a9caf7cb430774810c90c925ce6bd4cb8
Amrish <a.r.jhingoer@student.vu.nl>
2022-01-23 13:17:37 +0100

    upgraded docker to py3 with support for autoinstalling requirements on startup

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,6 @@ RUN apt-get update -y && apt-get install -y python-pip
 RUN pip install tensorflow keras
 WORKDIR /root/projects/
 RUN echo 'catkin_make install && source /root/projects/devel/setup.bash' >> /root/.bashrc
+ADD ./requirements.txt ./
+RUN python3 -m pip install -r requirements.txt
+RUN echo "----------READY for USE but this wont be printed :P----------"
--- src/robobo/__init__.py
+++ src/robobo/__init__.py
@@ -3,8 +3,13 @@ from __future__ import absolute_import, print_function
 from .simulation import SimulationRobobo
 from .simulation_prey import SimulationRoboboPrey
 
-import sys
-if sys.version_info < (3,0):
-    from .hardware import HardwareRobobo
-else:
-    print("Hardware Connection not available in python3 :(", file=sys.stderr)
+from .hardware import HardwareRobobo
+
+# See:
+# https://canvas.vu.nl/courses/56526/discussion_topics/479043
+
+#import sys
+#if sys.version_info < (3,0):
+#    from .hardware import HardwareRobobo
+#else:
+#    print("Hardware Connection not available in python3 :(", file=sys.stderr)
