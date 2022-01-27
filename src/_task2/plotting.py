7a7c0d755b05bece89abbdb97164fd41ca9c07ca
toetsie09 <s.brinkhuijsen@hotmail.com>
2022-01-27 22:32:26 +0100

    plot file

--- src/_task2/plotting.py
+++ src/_task2/plotting.py
@@ -1,135 +1,131 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
+import seaborn as sns
+from scipy.signal import savgol_filter
 
-def bar_plot(data, title='', xlabel='', ylabel='', legend_labels=['DDPG', 'DQN'], colors=['r','b'], 
-                save_fig=False, file_name='', file_location='./src/figures/'):
-    """
-        This function is used to create a bar plot for plotting the completion rate
-        Inputs
-            data: list of lists of lists
-                ->  [
-                        algorithm 1 [value1, value2, ..., valueN],
-                        algorithm 2 [value1, value2, ..., valueN]
-                    ]
-            title (string): name of the figure
-            xlabel (string): label of x-axis
-            ylabel (string): label of y-axis
-            legend_labels (list): the names of the algorithms for the legend(make sure to use the right order)
-            colors (list): the colors of the respective algorithms
-            save_fig (boolean): saves the figure
-            file_name (string): name of the file
-            file_location (string): target location of the file             
-    """
-    fig, ax = plt.subplots()
+def list_to_numpy(data):
+    new_data = [np.array(i) for i in data]
+    return np.array(new_data)
 
-    new_data = [x.sum()/x.shape[0] for x in data]
+def count_food(data):
+    new_data = [np.count_nonzero(np.array(i) == 100) for i in data]
+    return np.array(new_data)
 
-    ax.bar(legend_labels, new_data)
+def count_survived(data, food_mask, max_food, n_steps):
+    new_data = []
+    for i in data:
+        if food_mask == max_food:
+            new_data.append(n_steps)
+        else:
+            new_data.append(len(i))
 
-    ax.set_title(title)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
+    # new_data = [len(i) if np.count_nonzero(np.array(i) == 100) == max_food else n_steps for i in data]
+    return np.array(new_data)
 
-    if save_fig:
-        plt.savefig(file_location + file_name + '.png', bbox_inches='tight')
+def training_line_plot(rewards, food, title, xlabel, ylabels):
+    fig, ax = plt.subplots(2, sharex=True)
 
-    plt.legend()
+    x = np.arange(300)
+    sum_reward = [np.sum(row) for row in rewards]
+    # mean_reward = [np.mean(row) for row in rewards]
+    ax[0] = sns.lineplot(x, savgol_filter(sum_reward, 51, 3), ax=ax[0], color='blue')
+    ax[0] = sns.lineplot(x, sum_reward, ax=ax[0], alpha=0.5, color='darkblue')
+    # ax[0] = sns.lineplot(x, mean_reward, ax=ax[0])
+    ax[1] = sns.lineplot(x, savgol_filter(food, 51, 3), ax=ax[1], color='green')
+    ax[1] = sns.barplot(x, food, saturation=0.75, ax=ax[1], color='darkgreen', alpha=0.5)
+
+    fig.suptitle(title)
+    # ax.set_xlabel(xlabel)
+    ax[0].set_ylabel(ylabels[0])
+    ax[1].set_ylabel(ylabels[1])
+    ax[1].set_xlabel(xlabel)
+    fig.align_ylabels(ax[:])
+    plt.xticks([0, 50, 100, 150, 200, 250, 300])
     plt.show()
 
-def line_plot_simple(data, title='', xlabel='', ylabel='', legend_labels=['DDPG', 'DQN'], colors=['r','b'], 
-                save_fig=False, file_name='', file_location='./src/figures/'):
-    """
-        This function is used to create a line plot, for example for plotting the collected food and reward per episode.
-        Inputs
-            data: list of lists
-                ->  [
-                        algorithm 1 [value1, value2, ..., valueN],
-                        algorithm 2 [value1, value2, ..., valueN]
-                    ]
-            title (string): name of the figure
-            xlabel (string): label of x-axis
-            ylabel (string): label of y-axis
-            legend_labels (list): the names of the algorithms for the legend(make sure to use the right order)
-            colors (list): the colors of the respective algorithms
-            save_fig (boolean): saves the figure
-            file_name (string): name of the file
-            file_location (string): target location of the file             
-    """
-    fig, ax = plt.subplots()
+def validation_box_plot(DDPG, DQN, title):
+    fig, (ax1, ax2, ax3) = plt.subplots(1,3)
 
-    for i, line in enumerate(data):
-        plt.plot(line, colors[i], label=legend_labels[i])
+    ax1 = sns.boxplot(data=[DQN[0], DDPG[0]], ax=ax1)
+    ax1.set_xticklabels(['DQN', 'DDPG'])
+    ax1.set_xlabel('Algorithm')
+    ax1.set_ylabel('Survival rate')
 
-    ax.set_title(title)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
+    sum_reward_dqn = [np.sum(row) for row in DQN[1]]
+    sum_reward_ddpg = [np.sum(row) for row in DDPG[1]]
 
-    if save_fig:
-        plt.savefig(file_location + file_name + '.png', bbox_inches='tight')
+    ax2 = sns.boxplot(data=[sum_reward_dqn, sum_reward_ddpg], ax=ax2)
+    ax2.set_xticklabels(['DQN', 'DDPG'])
+    ax2.set_xlabel('Algorithm')
+    ax2.set_ylabel('Reward')
 
-    plt.legend()
+    ax3 = sns.boxplot(data=[DQN[2], DDPG[2]], ax=ax3)
+    ax3.set_xticklabels(['DQN', 'DDPG'])
+    ax3.set_xlabel('Algorithm')
+    ax3.set_ylabel('Collected Food')
+    fig.suptitle(title)
     plt.show()
+    print()
 
-def line_plot_advanced(data, title='', xlabel='', ylabel='', legend_labels=['DDPG', 'DQN'], colors=['r','b'], 
-                save_fig=False, file_name='', file_location='./src/figures/'):
-    """
-        This function is used to create a line plot with the standard deviation, 
-        for example for plotting the collected food and reward per episode.
-        Inputs
-            data: list of list
-                ->  [
-                        algorithm 1 [
-                            Trial 1 [value1, value2, ..., valueN],
-                            Trial 2 [value1, value2, ..., valueN],
-                            Trial N [value1, value2, ..., valueN],
-                            ]
 
-                        algorithm 2 [
-                            Trial 1 [value1, value2, ..., valueN],
-                            Trial 2 [value1, value2, ..., valueN],
-                            Trial N [value1, value2, ..., valueN],
-                            ]
-                    ]
-            title (string): name of the figure
-            xlabel (string): label of x-axis
-            ylabel (string): label of y-axis
-            legend_labels (list): the names of the algorithms for the legend(make sure to use the right order)
-            colors (list): the colors of the respective algorithms
-            save_fig (boolean): saves the figure
-            file_name (string): name of the file
-            file_location (string): target location of the file             
-    """
-    fig, ax = plt.subplots()
+# def validation_line_plot(DQN, DDPG, title, xlabel, ylabel):
+#     fig, ax = plt.subplots()
 
-    for i, line in enumerate(data):
-        x = np.arange(len(line[0]))
-        mean = np.mean(line, axis=0)
-        std = np.std(line, axis=0)
-        plt.plot(x, mean, colors[i], label=legend_labels[i])
-        plt.fill_between(x, mean-std, mean+std, alpha=0.2)
+#     x = np.arange(50)
+#     sum_reward_dqn = [np.sum(row) for row in DQN]
+#     sum_reward_ddpg = [np.sum(row) for row in DDPG]
+#     # mean_reward = [np.mean(row) for row in rewards]
+#     ax = sns.lineplot(x, sum_reward_dqn, ax=ax, alpha=0.8, color='blue')
+#     ax = sns.lineplot(x, sum_reward_ddpg, ax=ax, alpha=0.8, color='green')
 
-    ax.set_title(title)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
+#     fig.suptitle(title)
+#     # ax.set_xlabel(xlabel)
+#     ax.set_ylabel(ylabel)
+#     ax.set_xlabel(xlabel)
+#     # plt.xticks([0, 50, 100, 150, 200, 250, 300])
+#     plt.tight_layout()
+#     plt.show()
 
-    if save_fig:
-        plt.savefig(file_location + file_name + '.png', bbox_inches='tight')
-
-    plt.legend()
-    plt.show()
 
 def main():
-    x = np.random.choice(a=[False, True], size=100, p=[0.6, 0.4])
-    y = np.random.choice(a=[False, True], size=100, p=[0.7, 0.3])    
-    bar_plot([x,y])
+    path_dqn = './src/_task2/DQN/results/'
+    path_ddpg = './src/_task2/DDPG/results/'
 
-    x = [7, 14, 21, 28, 35, 42, 49]
-    y = [5, 12, 19, 21, 31, 27, 35]
-    line_plot_simple([x,y])
+    # with open(path_dqn + 'DQN_training_rewards_v1.pkl', 'rb') as file:
+    #     rewards_training_DQN = list_to_numpy(pickle.load(file))
+    #     padding = np.zeros(29)
+    #     rewards_training_DQN = np.append(rewards_training_DQN, padding)
+    #     foods_training_DQN = count_food(rewards_training_DQN)
 
-    E = np.random.normal((3, 5, 4), (1.25, 1.00, 0.25), (100, 3))
-    D = np.random.normal((6, 8, 2), (2, 3, 1), (100, 3))
-    line_plot_advanced([E, D])
+    # with open(path_ddpg + 'DDPG_training_rewards_final.pkl', 'rb') as file:
+    #     rewards_training_DDPG = list_to_numpy(pickle.load(file))
+
+    # with open(path_ddpg + 'DDPG_training_collected_foods.pkl', 'rb') as file:
+    #     foods_training_DDPG = list_to_numpy(pickle.load(file))
+
+    # # line_plot_advanced([food_training_DDPG, rewards_training_DDPG])
+    # training_line_plot(rewards_training_DDPG, foods_training_DDPG, title='Training DDPG', xlabel='#Episodes', ylabels=['Reward', 'Food Collected'])
+
+    # training_line_plot(rewards_training_DQN, foods_training_DQN, title='Training DQN', xlabel='#Episodes', ylabels=['Reward', 'Food Collected'])
+
+    with open(path_dqn + 'DQN_test_collected_foods_v1.pkl', 'rb') as file:
+        foods_test_DQN = pickle.load(file)
+
+    with open(path_ddpg + 'DDPG_evaluation_collected_foods.pkl', 'rb') as file:
+        foods_test_DDPG = pickle.load(file)
+
+    with open(path_dqn + 'DQN_test_rewards_v1.pkl', 'rb') as file:
+        rewards_test_DQN = pickle.load(file)
+    survival_rate_DQN = count_survived(rewards_test_DQN, foods_test_DQN, 7, 300)
+
+    with open(path_ddpg + 'DDPG_evaluation_rewards.pkl', 'rb') as file:
+        rewards_test_DDPG = pickle.load(file)
+    survival_rate_DDPG = count_survived(rewards_test_DDPG, foods_test_DDPG, 8, 300)
+
+    validation_box_plot([survival_rate_DDPG, rewards_test_DDPG, foods_test_DDPG], [survival_rate_DQN, rewards_test_DQN, foods_test_DQN], title='')
+    # validation_line_plot(rewards_test_DQN, rewards_test_DDPG, title='', xlabel='#Episodes', ylabel='Reward')
+    print()
 
 if __name__ == "__main__":
     main()
